12131c5cde632dd01e3a468ae4d466e69c0b7ecf
Harish <115090388+Rish-Bot@users.noreply.github.com>
2025-08-17 21:42:23 +0530

    Time: 2 ms (83.48%) | Memory: 45.9 MB (43.15%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        // let's check something new on leetsync
+        // let's check something new on leetsync, notes are not inclucded.
         int maxIndex = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            if (i > maxIndex) 
+            if (i > maxIndex)
                 return false;
-            
+
             maxIndex = Math.max(maxIndex, i + nums[i]);
         }
 
